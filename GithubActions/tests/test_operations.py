baef2e2471f6f24e7c6ad97a116a4bc8fd2311c1
Sanket Zanwar <shankii@Sankets-MacBook-Air.local>
2025-02-10 18:05:31 -0500

    unit test cases updated

---
+++ GithubActions/tests/test_operations.py
@@ -0,0 +1,12 @@
+from src.math_operations import add, sub
+
+
+def test_add():
+    assert add(2, 5) == 7
+    assert add(2, -2) == 0
+    assert add(2, 0) == 2
+
+
+def test_sub():
+    assert sub(1, 2) == -1
+    assert sub(1, -1) == 2
